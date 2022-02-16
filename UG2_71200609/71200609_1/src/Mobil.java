25d346a738cd2c16d7079afe2f5ccb25f431caea
AirellAristo <78353843+AirellAristo@users.noreply.github.com>
2022-02-16 18:42:50 +0800

    Add files via upload

---
+++ UG2_71200609/71200609_1/src/Mobil.java
@@ -0,0 +1,13 @@
+public class Mobil {
+    private String jenis;
+    private String mesin;
+    public static void jalan(){
+        System.out.println("Mobil Ngeng...");
+    }
+    public static void kurangiKecepatan(){
+        System.out.println("Mobil menguragi kecepatan...");
+    }
+    public static void berhenti(){
+        System.out.println("Mobil stop...");
+    }
+}
---
+++ UG2_71200609/71200609_1/src/main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class main {
+    Mobil M_71200609 = new Mobil();
+    PejalanKaki P_71200609 = new PejalanKaki();
+
+    public static void main(String[] args){
+    Scanner inp = new Scanner(System.in);
+    System.out.println("Kode lampu lalu lintas saat ini:");
+    int KodeLampu = inp.nextInt();
+    if(KodeLampu == 3){
+        System.out.println("-----Lampu Merah-----");
+        Mobil.berhenti();
+        PejalanKaki.menyebrang();}
+    if(KodeLampu == 2){
+        System.out.println("-----Lampu Kuning-----");
+        Mobil.kurangiKecepatan();
+        PejalanKaki.bersiap();}
+    if(KodeLampu == 1){
+        System.out.println("-----Lampu Hijau-----");
+        Mobil.jalan();
+        PejalanKaki.menyebrang();}
+    }
+
+}
